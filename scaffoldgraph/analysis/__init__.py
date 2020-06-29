aff27c760df6b34f62fe1f6df5e3062149a16d2c
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2020-06-29 16:14:39 +0100

    Added general analysis functions for identifying 'virtual' scaffolds and singletons

--- scaffoldgraph/analysis/__init__.py
+++ scaffoldgraph/analysis/__init__.py
@@ -3,10 +3,13 @@ scaffoldgraph.analysis
 """
 
 from .representation import calc_average_pairwise_similarity, get_over_represented_scaffold_classes
+from .general import get_virtual_scaffolds, get_singleton_scaffolds
 from .cse import CSE
 
 __all__ = [
     'calc_average_pairwise_similarity',
     'get_over_represented_scaffold_classes',
+    'get_virtual_scaffolds',
+    'get_singleton_scaffolds',
     'CSE',
 ]
