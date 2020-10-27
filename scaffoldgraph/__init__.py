003eebd9e791d675a96d33fcddd2fe2abb7fe541
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2020-10-27 11:46:38 +0000

    add rdversion to __init__ + cleanup

--- scaffoldgraph/__init__.py
+++ scaffoldgraph/__init__.py
@@ -2,18 +2,29 @@
 scaffoldgraph
 """
 
+import rdkit
+
 from loguru import logger
 
 from . import prioritization
 from . import utils
-from .core import get_next_murcko_fragments, get_all_murcko_fragments, get_murcko_scaffold
+
+from .core import (
+    get_next_murcko_fragments,
+    get_all_murcko_fragments,
+    get_murcko_scaffold,
+)
+
 from .network import ScaffoldNetwork, HierS
 from .tree import ScaffoldTree, tree_frags_from_mol
 
 __version__ = '1.0.3'
 
+rdversion = rdkit.__version__
+
 __all__ = [
     '__version__',
+    'rdversion',
     'HierS',
     'ScaffoldNetwork',
     'ScaffoldTree',
