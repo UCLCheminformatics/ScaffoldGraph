9be3165ca7de1ae051db8982ef64accfc4fa0bdf
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2020-12-17 15:39:21 +0000

    Add supress_rdlogger to utils __init__.py

--- scaffoldgraph/utils/__init__.py
+++ scaffoldgraph/utils/__init__.py
@@ -4,9 +4,11 @@ scaffoldgraph.utils
 
 from .misc import canonize_smiles, summary
 from .aggregate import aggregate
+from .logging import supress_rdlogger
 
 __all__ = [
     'canonize_smiles',
     'aggregate',
-    'summary'
+    'summary',
+    'supress_rdlogger',
 ]
