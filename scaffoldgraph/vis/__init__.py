55411995af516f716f12997241c14426b3b90a5d
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2021-01-06 15:18:47 +0000

    Update vis __init__.py

--- scaffoldgraph/vis/__init__.py
+++ scaffoldgraph/vis/__init__.py
@@ -2,9 +2,20 @@
 scaffoldgraph.vis
 """
 
-from .utils import embed_node_mol_images, remove_node_mol_images
+from .utils import (
+    embed_node_mol_images,
+    remove_node_mol_images,
+    color_scaffold_nodes_by_attribute,
+    color_molecule_nodes_by_attribute,
+    add_root_node,
+    remove_root_node,
+)
 
 __all__ = [
     'embed_node_mol_images',
     'remove_node_mol_images',
+    'color_scaffold_nodes_by_attribute',
+    'color_molecule_nodes_by_attribute',
+    'add_root_node',
+    'remove_root_node',
 ]
