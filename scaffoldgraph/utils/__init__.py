c0f7d8ea812c79b0be2d172e139218d710c68fc9
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2021-01-06 09:12:11 +0000

    Undo

--- scaffoldgraph/utils/__init__.py
+++ scaffoldgraph/utils/__init__.py
@@ -4,13 +4,11 @@ scaffoldgraph.utils
 
 from .misc import canonize_smiles, summary
 from .aggregate import aggregate
-from .bipartite import make_bipartite_graph
 from .logging import suppress_rdlogger
 
 __all__ = [
     'canonize_smiles',
     'aggregate',
     'summary',
-    'make_bipartite_graph',
     'suppress_rdlogger',
 ]
