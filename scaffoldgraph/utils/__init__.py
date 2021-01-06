60c778f3ccbe6da55bf96a1a5e808d63a0a22cf2
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2021-01-06 09:10:14 +0000

    Add make_bipartite_graph to utils __init__.py

--- scaffoldgraph/utils/__init__.py
+++ scaffoldgraph/utils/__init__.py
@@ -4,11 +4,13 @@ scaffoldgraph.utils
 
 from .misc import canonize_smiles, summary
 from .aggregate import aggregate
+from .bipartite import make_bipartite_graph
 from .logging import suppress_rdlogger
 
 __all__ = [
     'canonize_smiles',
     'aggregate',
     'summary',
+    'make_bipartite_graph',
     'suppress_rdlogger',
 ]
