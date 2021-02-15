fd882340442a19667c77a58ab93f49e1041a21e2
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2021-02-15 12:11:01 +0000

    prepare for pip version update.

--- scaffoldgraph/__init__.py
+++ scaffoldgraph/__init__.py
@@ -19,7 +19,7 @@ from .core import (
 from .network import ScaffoldNetwork, HierS
 from .tree import ScaffoldTree, tree_frags_from_mol
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 
 __all__ = [
