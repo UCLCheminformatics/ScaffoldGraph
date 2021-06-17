a5a45bc1aad7c308994b3b7724b4437f6b37216b
Oliver Scott <oliver.scott.17@ucl.ac.uk>
2021-06-17 11:56:40 +0100

    v1.1.2

--- scaffoldgraph/__init__.py
+++ scaffoldgraph/__init__.py
@@ -19,7 +19,7 @@ from .core import (
 from .network import ScaffoldNetwork, HierS
 from .tree import ScaffoldTree, tree_frags_from_mol
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 
 __all__ = [
